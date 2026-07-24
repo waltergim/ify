ba91fe899cc67de2ea89aaf458e092fdff66eae0
walter gimenes <waltergim49@gmail.com>
2026-07-24 12:58:55 -0300

    Elimino archivo con credenciales expuestas

--- ify-api/prisma.config.ts
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-
-export default {
-  schema: "./prisma/schema.prisma",
-  datasource: {
-    url: "postgresql://postgres.ctfxpofmeqjqdhggktao:Ify2026202520@aws-1-us-west-2.pooler.supabase.com:5432/postgres"
-  }
-};
